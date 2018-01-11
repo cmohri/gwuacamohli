d5cd886ed75f485508b9a6b92e602d1b82ace5f5
Clara Mohri <claramohri@napoleon.fios-router.home>
2018-01-10 22:21:46 -0500

    Update Grapher.java

--- v2/Grapher.java
+++ v2/Grapher.java
@@ -18,26 +18,36 @@ public class Grapher
 	ymax = 25;
 	xmin = 0;
 	ymin = 0;
-	_graph = new String[ymax -ymin+1 ][xmax - xmin+1 ];
+	_graph = new String[ymax -ymin+2 ][xmax - xmin+2 ];
 	filler();
     }
 
     //overloaded constructor 
     //allows user to determine domain and range for viewing window
     public Grapher(int domain, int range){
-	_graph = new String[range][domain];
+	xmin = 0;
+	ymin = 0;
+	xmax = domain;
+	ymax = range;
+	_graph = new String[ymax -ymin+2 ][xmax - xmin+2 ];
 	filler();
     }
 
     //overloaded constructor
     //allows user to determine domain and range when not starting from 0
-    public Grapher(int xmin, int xmax, int ymin, int max){
+    public Grapher(int xmini, int xmaxi, int ymini, int ymaxi){
+	xmin = xmini;
+        ymin = ymini;
+        xmax = xmaxi;
+        ymax = ymaxi;
+        _graph = new String[ymax -ymin+2 ][xmax - xmin+2 ];
+        filler();
     }
 
     //filler method fills _graph with spaces 
     public void filler(){
-	for (int i = 0; i <  ymax; i++){
-	    for (int x = 0; x < xmax; x++){
+	for (int i = 0; i <=  ymax; i++){
+	    for (int x = 0; x <= xmax; x++){
 		_graph[i][x] = "  ";
 	    }
 	}
@@ -46,10 +56,10 @@ public class Grapher
     //toString method shows contents of _graph
     public String toString() {
         String foo = "";
-        for( int i = 0; i < ymax ; i++ ) {
+        for( int i = ymin; i <= ymax ; i++ ) {
 	    //y axis:
-	    foo += ymax-i - 1;
-	    if (ymax - 1 - i < 10)
+	    foo += ymax-i;
+	    if (ymax  - i < 10 && ymax-i >-1)
 		foo += " ";
             foo += "| ";
             for( int j=0; j < xmax; j++ ) {
@@ -59,7 +69,7 @@ public class Grapher
         }	
 	foo += "    ";
 	//x axis bar:
-	for (int i = xmin; i < xmax; i++){
+	for (int i = xmin; i <= xmax; i++){
 	    if (i/2 == 0)  
 		foo += "–––";
 	    else 
@@ -68,7 +78,7 @@ public class Grapher
 	foo += "\n";
 	//x axis:
 	foo += "     ";
-	for (int i = xmin; i < xmax; i++){
+	for (int i = xmin; i <= xmax; i++){
 	    foo += i;
 	    foo += "  ";
 	}
@@ -85,7 +95,7 @@ public class Grapher
 	for (int x = xmin; x < xmax; x++){
 	    int y = Parser.input(eq, x);
 	    if (y < ymax && y >= ymin){
-		_graph[ymax-1-y][x] = " •";
+		_graph[ymax-y][x] = " •";
 	    }
 	}
 	System.out.println(this);
@@ -94,7 +104,7 @@ public class Grapher
 
     public static void main (String[] args){
 
-	Grapher foo = new Grapher();
+	Grapher foo = new Grapher(/*-10,10, -10,10*/);
 	//System.out.println(foo);
 	foo.grapher();
 	//System.out.println(foo);
