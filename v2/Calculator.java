6fe1cf67deecd6d3c0704a65ec703c14f74d313b
Jessica Wu <jessicawu49260@gmail.com>
2018-01-11 23:50:04 -0500

    Calculator can take in exponents now

--- v2/Calculator.java
+++ v2/Calculator.java
@@ -1,11 +1,10 @@
 /* to-do
    1. create method to parse by * and / --> return output into a new arraylist 
    to evaluate first (PEMDAS) and then parse + evaluate by +/-
-   2. what to do w/ exponents??? --> use parser methods??
 */
 
 import cs1.Keyboard;
-import java.util.ArrayList;
+import java.lang.Math;
 
 public class Calculator {
 
@@ -15,23 +14,50 @@ public class Calculator {
 	return output;
     }//end of parse()
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //finds the base of every term of the expression
+    public static int findBase(String term){
+	int retVal = 0;
+	int carrot = term.indexOf("^");
+	//System.out.println(carrot);
+	if (carrot >= 0 && (carrot< term.length())){
+	    String base = term.substring(0, carrot);
+	    retVal = Integer.parseInt(base);
+	}
+	return retVal;
+    }
     
     //Calculator
     public static double Calculator(String expression){
 
-	double final_output = 0;
+	int final_output;
 	String[] temparray;
 	int oneafter;
 	int num;
 
 	temparray = parse(expression);
-	final_output = Integer.parseInt(temparray[0]);
 	
 	// for (int i =0;i < temparray.length;i++){
 	//     System.out.println(temparray[i]);
 	// }
 	
 	//for loop goes through entire parsed array
+	for (int i = 0; i < temparray.length; i++){
+	    //if "^" --> 1. get degree 2. get base number 3. raise base to degree
+	    if (temparray[i].indexOf("^") >= 0){
+		//System.out.println("carrot");
+		int expt = Parser.findDeg(temparray[i]);
+		//System.out.println(deg);
+		int base = findBase(temparray[i]);
+	      	//System.out.println(base);
+		int replacement = (int)( Math.pow(base,expt));
+		//System.out.println(replacement);
+		temparray[i] = Integer.toString(replacement);
+	    }
+	}
+
+	final_output = Integer.parseInt(temparray[0]);
+	
 	for (int i = 0; i < temparray.length; i++){
 	    //if "+" --> add() the value at i + 1
 	    if (temparray[i].equals("+")){
@@ -62,6 +88,7 @@ public class Calculator {
 		final_output /= num;
 	    }
 	}
+    
 
 	return final_output;
     }//end Calculator
@@ -85,8 +112,10 @@ public class Calculator {
 	//	System.out.println(Calculator("3+1"));
 
 	Calculator foo = new Calculator();
-	System.out.println("Enter a math expression. \n\t1. No exponents \n\t2. Place a space between each operation (+,-,*,/). Make sure you don't have an extra space at the end!"); 
+	System.out.println("Enter a math expression. \n\t1. Place a space between each operation (+,-,*,/). Make sure you don't have an extra space at the end! \n\t2. If you wish to use exponents, don't include spaces between the base, '^', and exponent. For example: 3^2"); 
 	String expression = Keyboard.readString();
+	//int deg = Parser.findDeg(expression);
+	//System.out.println(deg);
 	System.out.println(foo.Calculator(expression));
     }
     
