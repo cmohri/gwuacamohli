1a137620fcb5e02d29c1189c68449feb9dda0cec
Clara Mohri <claramohri@napoleon.fios-router.home>
2018-01-15 21:58:04 -0500

    Update Woo.java

--- v2/Woo.java
+++ v2/Woo.java
@@ -5,12 +5,13 @@ public class Woo{
 
     //driver method calls on other classes based on user selection
     public static void driver() {
-	System.out.println("Mathematical Utility");
+
 	System.out.println("Select functionality");
-	System.out.println("1. Function work");
+	System.out.println("1. Equation graphing, statistics, more");
 	System.out.println("2. Open spreadsheet");
 	System.out.println("3. Calculator");
-	System.out.println("4. Exit");
+	System.out.println("4. View description of each option");
+	System.out.println("5. Exit");
 	int userInput = Keyboard.readInt();
 	while (userInput < 1 || userInput > 4){
 	    System.out.println("Please input a valid selection");
@@ -30,15 +31,35 @@ public class Woo{
 	    Calculator bar = new Calculator();
 	    bar.calculate();
 	}
+	if (userInput == 4){
+	    System.out.println(description());
+	}
 	else {	    
 	    return;
 	}
 	driver();
     }
     
-
-    public static void main(String[] args) {
-	driver();	
+    public static String description() {
+	String retStr = "";
+	retStr += ("------------------------------------------");
+	retStr += ("\nPROGRAM DESCRIPTION\n");
+	retStr += ("This program offers some of the functionality of a graphing calculator.\n");
+	retStr += ("1: Equation graphing, statistics, more\n");
+	retStr += ("\tEnter in a polynomial in terms of x\n\tCalculate intercepts\n\tView table\n\tChange viewing window\n\tOr download graph/table\n");
+	retStr += ("2: Open spreadsheet\n");
+	retStr += ("\tUse a spreadsheet to input values\n");
+	retStr += ("3: Calculator\n");
+	retStr += ("\tUse a basic calculator program\n");
+	retStr += ("------------------------------------------\n\n");
+	return retStr;
     }
+
+    //main method, executable
+    public static void main(String[] args) {
+	//to make text bold:
+	System.out.println("\n\n\033[1mMathematical Utility:\033[0m\n\n");
+	driver();	
+    }//end main method
     
 }//end of Driver
